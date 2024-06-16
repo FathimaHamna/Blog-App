6b75b86506405a546377b078582985ba1283b9f9
FathimaHamna <119462727+FathimaHamna@users.noreply.github.com>
2024-06-16 13:13:03 +0530

    responsive layout

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,5 +4,7 @@ import styles from "./homepage.module.css";
 export default function Home() {
   return <div>
     <Link href="/">
-    home</Link></div>;
+    <h1>Lorem Ipsum bdjbwjqbcqk dhfjjg djgjr skkahf Lorem Ipsum bdjbwjqbcqk dhfjjg djgjr skkah Lorem</h1>
+    Lorem ipsum nnnnnnnnnnmhj cdjfndnncmndskfnajbksrhfkuweb chqbcqbj hcdwgqb  rg a MGFJEQJC  hdsdh.Lorem Ipsum bdjbwjqbcqk dhfjjg djgjr skkahLorem Ipsum bdjbwjqbcqk dhfjjg djgjr skkahLorem Ipsum bdjbwjqbcqk dhfjjg djgjr skkahLorem Ipsum bdjbwjqbcqk dhfjjg djgjr skkahLorem Ipsum bdjbwjqbcqk dhfjjg djgjr skkahLorem Ipsum bdjbwjqbcqk dhfjjg djgjr skkahLorem Ipsum bdjbwjqbcqk dhfjjg djgjr skkahvLorem Ipsum bdjbwjqbcqk dhfjjg djgjr skkahLorem Ipsum bdjbwjqbcqk dhfjjg djgjr skkahLorem Ipsum bdjbwjqbcqk dhfjjg djgjr skkahLorem Ipsum bdjbwjqbcqk dhfjjg djgjr skkahLorem Ipsum bdjbwjqbcqk dhfjjg djgjr skkahLorem Ipsum bdjbwjqbcqk dhfjjg djgjr skkah</Link>
+    </div>;
 }

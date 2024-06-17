2cf570dfb21dc629ddce3a0f48c756f2f1d81a51
FathimaHamna <119462727+FathimaHamna@users.noreply.github.com>
2024-06-18 00:41:16 +0530

    side menu

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -16,10 +16,10 @@ const Card = () => {
         <Link href="/">
         <h1>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</h1>
         </Link>
-        <p>
+        <p className={styles.desc}>
         Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
         </p>
-        <Link href="/">Read More</Link>
+        <Link href="/" className={styles.link}>Read More</Link>
         </div>
     </div>
   )
